71ccf73e9c08f033feda6a63d17b45d34308ae0e
BastiaanvdB <70689873+BastiaanvdB@users.noreply.github.com>
2021-03-28 00:03:17 +0100

    week-5-A

---
+++ SomerenUI/LoginUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SomerenModel;
+using SomerenLogic;
+
+namespace SomerenUI
+{
+    public partial class LoginUI : Form
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+            MagicWord magic = new MagicWord();
+            magic.Show();
+        }
+
+
+        private void Login()
+        {
+            if ((textBoxUsername.Text.Length > 0) && (textBoxPassword.Text.Length > 0))
+            {
+                User user = new User
+                {
+                    Username = textBoxUsername.Text,
+                    Password = textBoxPassword.Text
+                };
+
+            }
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+    }
+}
